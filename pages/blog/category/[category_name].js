7482b8341eee52366240527c268bf00cd383570b
aurassai <b-raychev@yandex.com>
2021-10-04 16:09:07 +0300

    cleanup

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -1,12 +1,9 @@
 import fs from "fs";
 import path from "path";
-import Link from "next/link";
 import matter from "gray-matter";
 import Layout from "@/components/Layout";
 import Post from "@/components/Post";
-import { sortByDate } from "@/utils/index";
-import { POST_PER_PAGE } from "@/config/index";
-import Pagination from "@/components/Pagination";
+import { getPosts } from "@/lib/posts";
 
 export default function CategoryBlogPage({ posts, categoryName }) {
     return (
@@ -47,16 +44,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { category_name } }) {
-    const files = fs.readdirSync(path.join("posts"));
-    const posts = files.map((filename) => {
-        const slug = filename.replace(".md", "");
-        const markdownWithMeta = fs.readFileSync(
-            path.join("posts", filename),
-            "utf-8"
-        );
-        const { data: frontmatter } = matter(markdownWithMeta);
-        return { slug, frontmatter };
-    });
+    const posts = getPosts();
 
     // Filter posts by category
     const categoryPosts = posts.filter(
